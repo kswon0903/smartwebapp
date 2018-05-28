1cc3471cb0159f592a0b08bd7b157e21e63e5944
Unknown <kswon0903@hanmail.net>
2018-05-28 11:08:22 +0900

    javascripts 1회차 : 변수 string 예제

---
+++ javascript/day1/js/variable.js
@@ -0,0 +1,10 @@
+var str1 = "My name is";
+var str2 = 'java';
+var str3 = String ("Name");
+var str4 = new String ('Is');
+var str5 = str2.valueOf();
+
+console.log("str1 : "+str1);
+console.log("str2 : "+str2);
+
+console.log("str1 type : " + typeof str1);
